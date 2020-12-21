86ffb290f3820248fca232f83d3e4a77d6b74bbf
rachitgupta98 <rachit371@gmail.com>
2020-12-21 14:59:24 +0530

    add diff method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,13 @@ public class Calc {
 	public static int add(int a, int b) {
 		return a + b;
 	}
-
+	public static int diff(int a, int b) {
+		return a - b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(55, 45));
+		System.out.println("Sum is :"+add(55, 45));
+		System.out.println("Difference is :"+diff(55, 45));
 	}
 
 }
