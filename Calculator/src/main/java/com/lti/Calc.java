c4c396e776d6069d6a94742e9fe156fef57a3c5d
rachitgupta98 <rachit371@gmail.com>
2020-12-21 15:16:31 +0530

    add div method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,11 +11,15 @@ public class Calc {
 	public static int prod(int a, int b) {
 		return a*b;
 	}
+	public static int div(int a, int b) {
+		return a/b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum is :"+add(55, 45));
 		System.out.println("Difference is :"+diff(55, 45));
 		System.out.println("Product is :"+prod(5, 4));
+		System.out.println("Quotient is :"+div(50, 2));
 	}
 
 }
