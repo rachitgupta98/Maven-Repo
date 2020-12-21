d2073945eccf24e3a20c915a1646a1b9ef8986b5
rachitgupta98 <rachit371@gmail.com>
2020-12-21 15:10:47 +0530

    add prod method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,10 +8,14 @@ public class Calc {
 	public static int diff(int a, int b) {
 		return a - b;
 	}
+	public static int prod(int a, int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum is :"+add(55, 45));
 		System.out.println("Difference is :"+diff(55, 45));
+		System.out.println("Product is :"+prod(5, 4));
 	}
 
 }
