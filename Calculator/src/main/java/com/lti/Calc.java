48ce4eeefc5299a36fb3bc6e00c0f529b79e9bb2
rachitgupta98 <rachit371@gmail.com>
2020-12-21 12:57:50 +0530

    initial commit

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calc {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(55, 45));
+	}
+
+}
